a33f820bee633a5f16df4b591ce472a0d3e31e68
Krishan Mohan <krishanmohan3501@gmail.com>
2024-06-10 18:08:54 +0530

    updated folder

---
+++ JAVA_WITH_DSA_FOUNDATION_FREE BATCH/Pattern_Printing/Prob_1.java
@@ -0,0 +1,26 @@
+//package Pattern_Printing;
+//
+//public class Prob_1 {
+//    public static void main(String[] args) {
+//
+//        pattern_1(4);
+//
+//
+//    }
+//
+//
+//
+//
+//    // printing a square of star of n rows
+//    static void pattern_1(int n){
+//        for(int i = 0; i<n;i++){
+//            for(int j = 0; j<n;j++){
+//                System.out.print("* ");
+//            }
+//            System.out.println();
+//        }
+//    }
+//}
+//
+//    //
+//
